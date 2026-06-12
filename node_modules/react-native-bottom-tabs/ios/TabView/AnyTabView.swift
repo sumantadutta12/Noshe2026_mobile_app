08f53b8f5613df8f8c6c4cb2f7cb899b56168e26
Neelam Mahato <neelammahato3@gmail.com>
2026-06-12 12:23:05 +0530

    first commit

--- node_modules/react-native-bottom-tabs/ios/TabView/AnyTabView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-public protocol AnyTabView: View {
-  var onLayout: (_ size: CGSize) -> Void { get }
-  var onSelect: (_ key: String) -> Void { get }
-  var updateTabBarAppearance: () -> Void { get }
-}
--- node_modules/react-native-bottom-tabs/src/NativeSVGDecoder.ts
+++
@@ -1,5 +0,0 @@
-import { TurboModuleRegistry, type TurboModule } from 'react-native';
-
-export interface Spec extends TurboModule {}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('SvgDecoder');
